a3ed940623ee5b0283726a57ce0ef1c253ef0468
Polo1120 <edgardopolo37@gmail.com>
2025-08-25 23:26:16 -0500

    settings estrucura

--- src/index.ts
+++ src/index.ts
@@ -9,33 +9,20 @@ import { checkApiKey } from "./middlewares/checkApiKey";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 import * as OpenApiValidator from "express-openapi-validator";
+import path from "path";
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
 const PORT = process.env.PORT || 3000;
 
-const swaggerDocument = YAML.load("./openapi.yaml");
-
-
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-app.use((req, res, next) => {
-  if (req.path.startsWith("/docs")) {
-    return next();
-  }
-  return checkApiKey(req, res, next);
-});
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.use("/api/auth", authRoutes);
-app.use("/api/images", imageRoutes);
-
+const swaggerDocument = YAML.load(path.join(__dirname, "../openapi.yaml"));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(
   OpenApiValidator.middleware({
@@ -46,14 +33,16 @@ app.use(
   })
 );
 
+app.use("/api", checkApiKey);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/images", imageRoutes);
 
 app.get("/", (_req, res) => {
   res.send("🚀 Image processing service is running");
 });
 
-
 app.use(errorHandler);
-
 
 app.listen(PORT, () => {
   console.log(`✅ Server listening on port :${PORT}`);
