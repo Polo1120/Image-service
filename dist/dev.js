965028bc6df1aa4f7e169ecca164572e78eb77bb
Polo1120 <edgardopolo37@gmail.com>
2025-09-02 19:49:04 -0500

    settings

--- dist/dev.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./index");
-const PORT = process.env.PORT || 3000;
-index_1.app.listen(PORT, () => {
-    console.log(`🚀 Dev server running on http://localhost:${PORT}`);
-});
